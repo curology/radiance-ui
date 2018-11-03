f80250d3eda546d2b0c8f10e4b5e9b768bdd27da
Seiji Naganuma <snaganumas@gmail.com>
2018-11-02 20:50:18 -0700

    add alert test and setup snapshot testing

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,4 +1,11 @@
 import { configure } from 'enzyme';
+import * as emotion from 'emotion';
 import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'jest-emotion'
 
 configure({ adapter: new Adapter() });
+/* eslint-disable no-undef */
+expect.addSnapshotSerializer(createSerializer(emotion))
+window.__SVG_INLINE__ = true;
+/* eslint-enable no-undef */
+
