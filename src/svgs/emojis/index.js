3add7904d5f4b84274fdd99c4191929b9829f30c
Seiji Naganuma <snaganumas@gmail.com>
2018-11-14 12:18:35 -0800

    add all the emoji svg definitions

--- src/svgs/emojis/index.js
+++ src/svgs/emojis/index.js
@@ -1,9 +1,26 @@
 // @create-index
 
+export { default as AnxiousEmoji } from './anxious-emoji.svg';
+export { default as AnxiousFilledEmoji } from './anxious-filled-emoji.svg';
+export { default as ConfusedEmoji } from './confused-emoji.svg';
+export { default as ElatedEmoji } from './elated-emoji.svg';
+export { default as ElatedFilledEmoji } from './elated-filled-emoji.svg';
+export { default as FuriousEmoji } from './furious-emoji.svg';
+export { default as FuriousFilledEmoji } from './furious-filled-emoji.svg';
 export { default as HeartEmoji } from './heart-emoji.svg';
 export { default as HeartFilledEmoji } from './heart-filled-emoji.svg';
 export { default as HugEmoji } from './hug-emoji.svg';
 export { default as HugFilledEmoji } from './hug-filled-emoji.svg';
+export { default as OverwhelmedEmoji } from './overwhelmed-emoji.svg';
+export { default as OverwhelmedFilledEmoji } from './overwhelmed-filled-emoji.svg';
+export { default as PreparedEmoji } from './prepared-emoji.svg';
+export { default as PreparedFilledEmoji } from './prepared-filled-emoji.svg';
+export { default as SatisfiedEmoji } from './satisfied-emoji.svg';
+export { default as SatisfiedFilledEmoji } from './satisfied-filled-emoji.svg';
 export { default as SparkleEmoji } from './sparkle-emoji.svg';
 export { default as SparkleFiledEmoji } from './sparkle-filed-emoji.svg';
+export { default as SupportedEmoji } from './supported-emoji.svg';
+export { default as SupportedFilledEmoji } from './supported-filled-emoji.svg';
+export { default as UnsatisfiedEmoji } from './unsatisfied-emoji.svg';
+export { default as UnsatisfiedFilledEmoji } from './unsatisfied-filled-emoji.svg';
 
