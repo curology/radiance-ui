21b1f442b86f0a9cb7b1114de9416f039324fa36
Sara Brandes <sarabrandes@Saras-MacBook-Pro.local>
2019-01-25 13:28:03 -0800

    updated modal / immersiveModal to emotion 10

--- src/shared-components/modal/style.js
+++ src/shared-components/modal/style.js
@@ -1,4 +1,4 @@
-import styled from 'react-emotion';
+import styled from '@emotion/styled';
 
 import Typography from '../typography';
 import { COLORS, MEDIA_QUERIES, SPACING } from '../../constants';
