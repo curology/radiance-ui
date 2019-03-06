542e4238e08f1535c2ad7bb1986e612e74fb6109
Nate Harris <nate.harris@curology.com>
2019-03-06 13:22:55 -0800

    Update dropdown story

--- stories/dropdown/index.js
+++ stories/dropdown/index.js
@@ -3,9 +3,9 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withDocs } from 'storybook-readme';
 
-import DropdownReadme from '../../docs/dropdown/dropdown.md';
+import DropdownReadme from '../../docs/dropdown/main.md';
 import DropdownExample from './dropdownExample';
-import MobileReadme from '../../docs/dropdown/mobileDropdown.md';
+import MobileReadme from '../../docs/dropdown/mobile.md';
 import MobileExample from './mobileExample';
 
 const stories = storiesOf('Dropdown', module);
