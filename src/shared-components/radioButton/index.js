950938c52b5a62cd36d45dfa6bbf21b9967b4162
jcmustin <jonathan.mustin@gmail.com>
2019-03-08 17:27:24 -0800

    Added Icon support to checkbox and radio button.

--- src/shared-components/radioButton/index.js
+++ src/shared-components/radioButton/index.js
@@ -11,6 +11,7 @@ const propTypes = {
     PropTypes.node,
   ]),
   type: PropTypes.oneOf(['primary', 'secondary']),
+  Icon: PropTypes.node,
 };
 
 const defaultProps = {
--- src/shared-components/selectorButton/index.js
+++ src/shared-components/selectorButton/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css } from '@emotion/core';
+import {css} from '@emotion/core';
 
 import CheckmarkIcon from '../../svgs/icons/checkmark-icon.svg';
-import { COLORS } from '../../constants';
+import {COLORS} from '../../constants';
 import {
   OuterContainer,
   Selector,
@@ -21,10 +21,12 @@ const propTypes = {
   ]),
   type: PropTypes.oneOf(['primary', 'secondary']),
   selector: PropTypes.oneOf(['radio', 'checkbox']),
+  Icon: PropTypes.node,
 };
 
 const defaultProps = {
-  onClick: () => {},
+  onClick: () => {
+  },
   type: 'primary',
   selector: 'radio',
 };
@@ -35,6 +37,7 @@ const SelectorButton = ({
   children,
   type,
   selector,
+  Icon,
   ...rest
 }) => (
   <OuterContainer
@@ -45,15 +48,25 @@ const SelectorButton = ({
     {...rest}
   >
     <SelectorContainer>
-      <CheckmarkIcon
-        css={css`
+      {Icon ? (
+        <Icon
+          css={css`
           ${selectorIcon};
-          color: ${checked ? COLORS.white : 'transparent'};
         `}
-        width={11}
-        height={11}
-      />
-      <Selector type={type} checked={checked} selector={selector} />
+          width={11}
+          height={11}
+        />
+      ) : (
+        <CheckmarkIcon
+          css={css`
+            ${selectorIcon};
+            color: ${checked ? COLORS.white : 'transparent'};
+          `}
+          width={11}
+          height={11}
+        />
+      )}
+      <Selector type={type} checked={checked} selector={selector}/>
     </SelectorContainer>
 
     {children && (
