c5e93513f6e2494151a2e1f53dc08a98e6acc89f
Diego Fortes <daigof@gmail.com>
2019-05-02 16:36:49 -0300

    migrate Carousel to Action Button

--- src/shared-components/carousel/arrow/style.js
+++ src/shared-components/carousel/arrow/style.js
@@ -1,28 +1,18 @@
 import styled from '@emotion/styled';
 
-import { SPACING, COLORS, ANIMATION, Z_SCALE } from '../../../constants';
+import { SPACING, Z_SCALE } from '../../../constants';
 
 const ArrowContainer = styled.div`
-  cursor: pointer;
   position: absolute;
   top: 50%;
   z-index: ${Z_SCALE.e2000};
-  padding: ${SPACING.small};
-  background-color: ${COLORS.white};
-  border-radius: 50%;
-  border: 1px solid ${COLORS.border};
-  transform: scale(1, 1) translate(0%, -50%);
-  transition: all ${ANIMATION.defaultTiming};
+  transform: translate(0%, -50%);
   display: block;
 
   ${({ prev }) => prev && `left: ${SPACING.xsmall};`};
   ${({ next }) => next && `right: ${SPACING.xsmall};`};
 
   ${({ disabled }) => disabled && `display: none;`};
-
-  &:hover {
-    transform: scale(1.1, 1.1) translate(0%, -50%);
-  }
 `;
 
 export default ArrowContainer;
