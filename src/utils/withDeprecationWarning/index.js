e8d99a103fc1aa12ddca41af0248ffb62097e8a3
Diego Fortes <daigof@gmail.com>
2019-05-14 11:02:41 -0300

    add deprecation warning only to development, test

--- src/utils/withDeprecationWarning/index.js
+++ src/utils/withDeprecationWarning/index.js
@@ -1,8 +1,15 @@
+const allowedEnvironments = ['test', 'development', 'dev'];
+const conditional1 = allowedEnvironments.includes(process.env.NODE_ENV);
+
 export default function withDeprecationWarning(obj, deprecatedProperties = {}) {
   const handler = {
     get(target, property) {
-      if (Object.keys(deprecatedProperties).includes(property)) {
-        console.warn(`[Deprecation Warning]: ${deprecatedProperties[property]}`);
+      const conditional2 = Object.keys(deprecatedProperties).includes(property);
+      if (conditional1 && conditional2) {
+        // eslint-disable-next-line no-console
+        console.warn(
+          `[Deprecation Warning]: ${deprecatedProperties[property]}`
+        );
       }
 
       return target[property];
