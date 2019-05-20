0952364a95dc03a00a3a989ed510c50df1d5ab44
omar-b-c <omar.beglerovic@curology.com>
2019-05-20 20:18:49 +0200

    added test for content prop

--- src/shared-components/alert/test.js
+++ src/shared-components/alert/test.js
@@ -9,6 +9,7 @@ const testAlert = {
   type: 'success',
   duration: 'sticky',
 };
+const SampleContentComponent = () => (<div>Your info is updated!  <a href='http://google.com'> Click here to see changes</a></div>);
 
 describe('Alert UI snapshots', () => {
   test('renders success type and text', () => {
@@ -44,6 +45,20 @@ describe('Alert UI snapshots', () => {
       <Alert
         text={testAlert.text}
         type="info"
+        duration={testAlert.duration}
+        onExit={() => { }}
+      />
+    );
+
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders custom component passed in content prop', () => {
+    const component = renderer.create(
+      <Alert
+        content= {<SampleContentComponent />}
+        type="success"
         duration={testAlert.duration}
         onExit={() => { }}
       />
