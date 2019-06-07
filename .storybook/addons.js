69998d2d4fb5a31a41835839e349e7e4d85a896d
Michael Altamirano <michael.altamirano@curology.com>
2019-06-07 16:04:21 -0700

    add @storybook/addon-a11y

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,3 +3,4 @@ import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
 import 'storybook-readme/register';
 import '@storybook/addon-viewport/register';
+import '@storybook/addon-a11y/register';
