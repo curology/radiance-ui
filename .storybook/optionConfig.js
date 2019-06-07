f0622bed8cedd357190754ad7d267e276df60a21
Michael Altamirano <michael.altamirano@curology.com>
2019-06-07 16:54:29 -0700

    Add storybook-readme in global config (per v5 docs)

--- .storybook/optionConfig.js
+++ .storybook/optionConfig.js
@@ -1,8 +1,10 @@
 import { addParameters, addDecorator } from '@storybook/react';
 import Theme from './theme';
 import { withA11y } from '@storybook/addon-a11y';
+import { addReadme } from 'storybook-readme';
 
 addDecorator(withA11y);
+addDecorator(addReadme);
 
 addParameters({
   options: {
