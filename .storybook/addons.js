1403660ac8cbf5c578380146b829642c19b91047
Michael Altamirano <michael.altamirano@curology.com>
2019-06-07 16:54:10 -0700

    Reorder addon order (WIP, testing)

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,6 +1,6 @@
 import '@storybook/addon-options/register';
-import '@storybook/addon-knobs/register';
-import '@storybook/addon-actions/register';
 import 'storybook-readme/register';
-import '@storybook/addon-viewport/register';
+import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
+import '@storybook/addon-actions/register';
+import '@storybook/addon-viewport/register';
