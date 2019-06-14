54dab09983ae9bfbe1c0c709dffaae0f6c93591a
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-06-14 15:33:01 -0700

    Make README add-on panel last due to disuse

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,6 +1,6 @@
 import '@storybook/addon-options/register';
-import 'storybook-readme/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-viewport/register';
+import 'storybook-readme/register'; // Currently used in body of story only, not in add-on panel
