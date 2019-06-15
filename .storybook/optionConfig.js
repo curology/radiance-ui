e66f9f5aee48a731d4db16fee9128a9c804b385f
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-06-14 17:46:31 -0700

    Add padding around README config

--- .storybook/optionConfig.js
+++ .storybook/optionConfig.js
@@ -3,9 +3,13 @@ import Theme from './theme';
 import { withA11y } from '@storybook/addon-a11y';
 import { addReadme } from 'storybook-readme';
 import centered from '@storybook/addon-centered/react';
+import { configureReadme } from 'storybook-readme';
+import { css } from '@emotion/core';
 
 addDecorator(withA11y);
 addDecorator(addReadme);
+configureReadme({ DocPreview: ({ children }) => <div css={css`padding: 1rem;`}>{children}</div>, StoryPreview: ({ children }) => <div css={css`padding: 1rem;`}>{children}</div>})
+
 addDecorator(centered);
 
 addParameters({
