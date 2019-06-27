785fba88564ad9cc23f760cdff366688640ab5f1
Ben Kolde <benkolde@Bens-MacBook-Pro-3.local>
2019-06-27 09:38:06 -0700

    add monoFont declaration to Radiance

--- src/constants/fonts/index.js
+++ src/constants/fonts/index.js
@@ -3,4 +3,6 @@ export default {
     "'nocturno', Georgia, TimesNewRoman, 'Times New Roman', Times, Baskerville, serif",
   baseFont:
     "'larssiet', 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic, Verdana, sans-serif",
+  monoFont:
+  "'apercu mono', 'Lucida Console', Monaco, monospace",
 };
