1e0c2db2f0a3efbed30f43e819a758b8f589ad23
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-06-27 11:38:06 -0700

    Remove unused optionConfig import from storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,5 @@
 import { configure } from '@storybook/react';
 
-import optionConfig from './optionConfig';
-
 const req = require.context('../stories', true, /.js$/);
 function loadStories() {
   req.keys().forEach(req);
