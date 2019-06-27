9ac809d9e90a837e1f6f014d75c6958f1924d94a
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-06-27 11:37:39 -0700

    Replace remaining emotion injectGlobal with emotion/core Global component styling, rearrange decorators

--- .storybook/optionConfig.js
+++ .storybook/optionConfig.js
@@ -5,10 +5,73 @@ import { addReadme } from 'storybook-readme';
 import centered from '@storybook/addon-centered/react';
 import { configureReadme } from 'storybook-readme';
 import { Global, css } from '@emotion/core';
-import { resetStyles, brandStyles } from '../src/utils/injectGlobalStyles/style';
+import {
+  resetStyles,
+  brandStyles,
+} from '../src/utils/injectGlobalStyles/style';
 
+const InjectGlobalStyles = storyFn => (
+  <div>
+    <Global styles={resetStyles} />
+    <Global styles={brandStyles} />
+    <Global
+      styles={css`
+        @font-face {
+          font-family: 'nocturno';
+          src: url('https://s3-us-west-1.amazonaws.com/fonts-california.typotheque.com/WF-029669-009918-001560-fa6dd062b0c32d6d9a297bd175bb0381.eot');
+          src: url('https://s3-us-west-1.amazonaws.com/fonts-california.typotheque.com/WF-029669-009918-001560-fa6dd062b0c32d6d9a297bd175bb0381.eot?#iefix')
+              format('embedded-opentype'),
+            url('https://s3-us-west-1.amazonaws.com/fonts-california.typotheque.com/WF-029669-009918-001560-fa6dd062b0c32d6d9a297bd175bb0381.woff2')
+              format('woff2'),
+            url('https://s3-us-west-1.amazonaws.com/fonts-california.typotheque.com/WF-029669-009918-001560-fa6dd062b0c32d6d9a297bd175bb0381.woff')
+              format('woff'),
+            url('https://s3-us-west-1.amazonaws.com/fonts-california.typotheque.com/WF-029669-009918-001560-fa6dd062b0c32d6d9a297bd175bb0381.svg#Typotheque_webfonts_service')
+              format('svg');
+        }
+      `}
+    />
+    <Global
+      styles={css`
+        @font-face {
+          font-family: 'larssiet';
+          src: url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_1_0.eot');
+          src: url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_1_0.eot?#iefix')
+              format('embedded-opentype'),
+            url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_1_0.woff2')
+              format('woff2'),
+            url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_1_0.woff')
+              format('woff'),
+            url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_1_0.ttf')
+              format('truetype');
+        }
+      `}
+    />
+    <Global
+      styles={css`
+        @font-face {
+          font-family: 'larssiet';
+          font-weight: bold;
+          src: url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_0_0.eot');
+          src: url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_0_0.eot?#iefix')
+              format('embedded-opentype'),
+            url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_0_0.woff2')
+              format('woff2'),
+            url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_0_0.woff')
+              format('woff'),
+            url('https://s3.amazonaws.com/curology-cdn.com/fonts/larssiet/34535B_0_0.ttf')
+              format('truetype');
+        }
+      `}
+    />
+    {storyFn()}
+  </div>
+);
+
+addDecorator(InjectGlobalStyles);
+addDecorator(centered);
 addDecorator(withA11y);
 addDecorator(addReadme);
+
 configureReadme({
   DocPreview: ({ children }) => (
     <div
@@ -29,19 +92,6 @@ configureReadme({
     </div>
   ),
 });
-
-
-const InjectGlobalStyles = storyFn => (
-  <div>
-    <Global styles={resetStyles} />
-    <Global styles={brandStyles} />
-    {storyFn()}
-  </div>
-);
-
-addDecorator(InjectGlobalStyles);
-
-addDecorator(centered);
 
 addParameters({
   options: {
