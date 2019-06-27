c2f9631c199f011525316296a570ab6992e258ce
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-06-27 15:03:07 -0700

    Update emotion dependencies and usage (#124)
    * Replace emotion injectGlobalStyles styles usage with @emotion/core Global component
    
    * Replace remaining emotion injectGlobal with emotion/core Global component styling, rearrange decorators
    
    * Remove unused optionConfig import from storybook config
    
    * Remove now-unused framework-agnostic emotion dependency
    
    * Bump emotion dependencies to 10.0.14
    
    * Update snapshots (TODO: change non-dom attribute to camelCase)
    
    * Move optionConfig content directly into config, delete optionConfig

--- src/utils/injectGlobalStyles/index.js
+++
@@ -1,8 +0,0 @@
-import { injectGlobal } from 'emotion';
-
-import { resetStyles, brandStyles } from './style';
-
-export default function() {
-  injectGlobal(resetStyles);
-  injectGlobal(brandStyles);
-}
