9cff986f7457d24c3bc3685a486d35f42a2c6f0c
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-07-01 15:59:04 -0700

    Feature/expand jest config (#131)
    * Add collectCoverage options to jest.config.js
    
    * Alphabetize jest.config.js properties
    
    * Add minimum coverage threshold that currently passes tests
    
    * Use correct blob format

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,22 @@
-const { defaults } = require('jest-config');
-
 module.exports = {
-  snapshotSerializers: ['jest-emotion'],
-  setupFilesAfterEnv: ["<rootDir>tests/setupTests.js"],
-  testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, "/lib/"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/shared-components/**',
+    '/src/utils/**',
+    '!src/**/__snapshots__/*.js.snap',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 66,
+      lines: 75,
+      statements: 75,
+    },
+  },
   moduleNameMapper: {
     '\\.svg': '<rootDir>/tests/__mocks__/svgMock.js',
   },
+  setupFilesAfterEnv: ['<rootDir>tests/setupTests.js'],
+  snapshotSerializers: ['jest-emotion'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/__snapshots__'],
 };
