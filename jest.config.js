f974aa8053c530afcb954ee94eebb8ca2864dc9a
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-07-01 10:23:32 -0700

    Alphabetize jest.config.js properties

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/shared-components/**', '/src/utils/**', '!src/**/__snapshots__/**.js.snap'],
-  snapshotSerializers: ['jest-emotion'],
-  setupFilesAfterEnv: ['<rootDir>tests/setupTests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/__snapshots__'],
+  collectCoverageFrom: [
+    'src/shared-components/**',
+    '/src/utils/**',
+    '!src/**/__snapshots__/**.js.snap',
+  ],
   moduleNameMapper: {
     '\\.svg': '<rootDir>/tests/__mocks__/svgMock.js',
   },
+  setupFilesAfterEnv: ['<rootDir>tests/setupTests.js'],
+  snapshotSerializers: ['jest-emotion'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/__snapshots__'],
 };
