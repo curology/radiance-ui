6d9f04abfb8a3a2a55c1313be77bac331a5c77be
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-07-01 10:19:45 -0700

    Add collectCoverage options to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const { defaults } = require('jest-config');
-
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/shared-components/**', '/src/utils/**', '!src/**/__snapshots__/**.js.snap'],
   snapshotSerializers: ['jest-emotion'],
-  setupFilesAfterEnv: ["<rootDir>tests/setupTests.js"],
-  testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, "/lib/"],
+  setupFilesAfterEnv: ['<rootDir>tests/setupTests.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/__snapshots__'],
   moduleNameMapper: {
     '\\.svg': '<rootDir>/tests/__mocks__/svgMock.js',
   },
