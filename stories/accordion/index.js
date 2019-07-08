42058afbb9d48e8753ae54f9a00bac9a95b2ffef
Michael Altamirano <michaeljaltamirano@gmail.com>
2019-07-08 14:41:05 -0700

    Add subcomponents to Accordion story, refactor code grouping

--- stories/accordion/index.js
+++ stories/accordion/index.js
@@ -9,6 +9,58 @@ import { Accordion, Typography } from 'src/shared-components';
 import { SPACER } from 'src/constants';
 
 import DefaultAccordion from './defaultAccordion';
+import SubcomponentAccordions from './subcomponentAccordions';
+
+const subcomponents = (
+  <React.Fragment>
+    <Typography.Heading
+      css={css`
+        padding: ${SPACER.large} 0 ${SPACER.small};
+      `}
+    >
+      Subcomponents
+    </Typography.Heading>
+    <SubcomponentAccordions />
+  </React.Fragment>
+);
+
+const accordionWithKnobs = (
+  <React.Fragment>
+    <Typography.Heading
+      css={css`
+        padding: ${SPACER.small} 0 ${SPACER.small};
+      `}
+    >
+      With Knobs
+    </Typography.Heading>
+    <div
+      css={css`
+        width: 400px;
+      `}
+    >
+      <Accordion.Container>
+        <Accordion
+          title={
+            <Accordion.Content>
+              <Accordion.Truncate>
+                {text('Title text', 'Title')}
+              </Accordion.Truncate>
+            </Accordion.Content>
+          }
+          onClick={action('Accordion clicked')}
+          isOpen={boolean('isOpen', false)}
+          noBorder={boolean('noBorder', false)}
+          disabled={boolean('disabled', false)}
+          rightAlignArrow={boolean('rightAlignArrow', false)}
+        >
+          <Accordion.Content>
+            {text('Expanded text', 'Accordion expanded content')}
+          </Accordion.Content>
+        </Accordion>
+      </Accordion.Container>
+    </div>
+  </React.Fragment>
+);
 
 storiesOf('Accordion', module)
   .addDecorator(withKnobs)
@@ -21,40 +73,16 @@ storiesOf('Accordion', module)
             text-align: left;
           `}
         >
-          <DefaultAccordion />
           <Typography.Heading
             css={css`
-              padding: ${SPACER.large} 0 ${SPACER.medium};
+              padding: ${SPACER.small} 0 ${SPACER.small};
             `}
           >
-            With Knobs
+            Components
           </Typography.Heading>
-          <div
-            css={css`
-              width: 400px;
-            `}
-          >
-            <Accordion.Container>
-              <Accordion
-                title={
-                  <Accordion.Content>
-                    <Accordion.Truncate>
-                      {text('Title text', 'Title')}
-                    </Accordion.Truncate>
-                  </Accordion.Content>
-                }
-                onClick={action('Accordion clicked')}
-                isOpen={boolean('isOpen', false)}
-                noBorder={boolean('noBorder', false)}
-                disabled={boolean('disabled', false)}
-                rightAlignArrow={boolean('rightAlignArrow', false)}
-              >
-                <Accordion.Content>
-                  {text('Expanded text', 'Accordion expanded content')}
-                </Accordion.Content>
-              </Accordion>
-            </Accordion.Container>
-          </div>
+          <DefaultAccordion />
+          {subcomponents}
+          {accordionWithKnobs}
         </div>
       </React.Fragment>
     ))
