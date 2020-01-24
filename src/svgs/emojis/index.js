36cbbc265774daa2251c3122c12d4435a54a1e9b
Sara Brandes <sara.brandes@curology.com>
2020-01-24 14:15:13 -0800

    import all new emojis from figma

--- src/svgs/emojis/index.js
+++ src/svgs/emojis/index.js
@@ -3,6 +3,7 @@
 export { default as AnxiousEmoji } from './anxious-emoji.svg';
 export { default as AnxiousFilledEmoji } from './anxious-filled-emoji.svg';
 export { default as ConfusedEmoji } from './confused-emoji.svg';
+export { default as ConfusedFilledEmoji } from './confused-filled-emoji.svg';
 export { default as ElatedEmoji } from './elated-emoji.svg';
 export { default as ElatedFilledEmoji } from './elated-filled-emoji.svg';
 export { default as FuriousEmoji } from './furious-emoji.svg';
@@ -12,18 +13,30 @@ export { default as HeartFilledEmoji } from './heart-filled-emoji.svg';
 export { default as HugEmoji } from './hug-emoji.svg';
 export { default as HugFilledEmoji } from './hug-filled-emoji.svg';
 export { default as MildAcneEmoji } from './mild-acne-emoji.svg';
+export { default as MildAcneFilledEmoji } from './mild-acne-filled-emoji.svg';
 export { default as NoticeableAcneEmoji } from './noticeable-acne-emoji.svg';
+export {
+  default as NoticeableAcneFilledEmoji,
+} from './noticeable-acne-filled-emoji.svg';
 export { default as OverwhelmedEmoji } from './overwhelmed-emoji.svg';
-export { default as OverwhelmedFilledEmoji } from './overwhelmed-filled-emoji.svg';
+export {
+  default as OverwhelmedFilledEmoji,
+} from './overwhelmed-filled-emoji.svg';
 export { default as PreparedEmoji } from './prepared-emoji.svg';
 export { default as PreparedFilledEmoji } from './prepared-filled-emoji.svg';
 export { default as SatisfiedEmoji } from './satisfied-emoji.svg';
 export { default as SatisfiedFilledEmoji } from './satisfied-filled-emoji.svg';
 export { default as SevereAcneEmoji } from './severe-acne-emoji.svg';
+export {
+  default as SevereAcneFilledEmoji,
+} from './severe-acne-filled-emoji.svg';
+export { default as SmileyEmoji } from './smiley-emoji.svg';
+export { default as SmileyFilledEmoji } from './smiley-filled-emoji.svg';
 export { default as SparkleEmoji } from './sparkle-emoji.svg';
 export { default as SparkleFilledEmoji } from './sparkle-filled-emoji.svg';
 export { default as SupportedEmoji } from './supported-emoji.svg';
 export { default as SupportedFilledEmoji } from './supported-filled-emoji.svg';
 export { default as UnsatisfiedEmoji } from './unsatisfied-emoji.svg';
-export { default as UnsatisfiedFilledEmoji } from './unsatisfied-filled-emoji.svg';
-
+export {
+  default as UnsatisfiedFilledEmoji,
+} from './unsatisfied-filled-emoji.svg';
