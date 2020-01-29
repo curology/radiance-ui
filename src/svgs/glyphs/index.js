586df5e78eb22553639131ef7b991afe7245b8e6
Sara Brandes <smbrandes17@gmail.com>
2020-01-29 15:13:00 -0800

    [Icon Updates] Final 1/3 of Glyph updates from Figma (#177)
    * Added remaining glyphs from Figma
    
    * fix fills to accept passed color

--- src/svgs/glyphs/index.js
+++ src/svgs/glyphs/index.js
@@ -1,5 +1,7 @@
 // @create-index
 
+export { default as AcneBodySprayGlyph } from './acne-body-spray-glyph.svg';
+export { default as AcneBodyWashGlyph } from './acne-body-wash-glyph.svg';
 export { default as AcneFewGlyph } from './acne-few-glyph.svg';
 export { default as AcneFiveGlyph } from './acne-five-glyph.svg';
 export { default as AcneGlyph } from './acne-glyph.svg';
@@ -9,8 +11,12 @@ export { default as AcneSevenGlyph } from './acne-seven-glyph.svg';
 export { default as AcneSeveralGlyph } from './acne-several-glyph.svg';
 export { default as AcneSomeGlyph } from './acne-some-glyph.svg';
 export { default as AcneThreeGlyph } from './acne-three-glyph.svg';
+export {
+  default as AhaBhaTonerSolutionGlyph,
+} from './aha-bha-toner-solution-glyph.svg';
 export { default as AzelaicAcidGlyph } from './azelaic-acid-glyph.svg';
 export { default as BodyGlyph } from './body-glyph.svg';
+export { default as BodySunscreenGlyph } from './body-sunscreen-glyph.svg';
 export { default as CalendarAlwaysGlyph } from './calendar-always-glyph.svg';
 export {
   default as CalendarMostOfTheTimeGlyph,
@@ -22,6 +28,7 @@ export {
 export { default as ChatBubbleGlyph } from './chat-bubble-glyph.svg';
 export { default as CheckBadgeGlyph } from './check-badge-glyph.svg';
 export { default as ChestGlyph } from './chest-glyph.svg';
+export { default as CleanserGlyph } from './cleanser-glyph.svg';
 export { default as ClindamycinGlyph } from './clindamycin-glyph.svg';
 export { default as ClipboardStackGlyph } from './clipboard-stack-glyph.svg';
 export { default as CloggedPoresGlyph } from './clogged-pores-glyph.svg';
@@ -30,18 +37,31 @@ export { default as ConsultHistoryGlyph } from './consult-history-glyph.svg';
 export { default as CourseGlyph } from './course-glyph.svg';
 export { default as DarkSpotsGlyph } from './dark-spots-glyph.svg';
 export { default as DropperGlyph } from './dropper-glyph.svg';
+export { default as ExfoliatingMaskGlyph } from './exfoliating-mask-glyph.svg';
+export { default as EyecreamGlyph } from './eyecream-glyph.svg';
 export { default as FaceGlyph } from './face-glyph.svg';
 export { default as FemaleGlyph } from './female-glyph.svg';
 export { default as FineLinesGlyph } from './fine-lines-glyph.svg';
 export { default as FirmnessGlyph } from './firmness-glyph.svg';
+export { default as FlagCanadaGlyph } from './flag-canada-glyph.svg';
+export { default as FlagUKGlyph } from './flag-u-k-glyph.svg';
+export { default as FlagUSAGlyph } from './flag-u-s-a-glyph.svg';
 export {
   default as GeneralQuestionsGlyph,
 } from './general-questions-glyph.svg';
+export { default as GlobeGlyph } from './globe-glyph.svg';
 export { default as HandWritingGlyph } from './hand-writing-glyph.svg';
+export { default as HydratingMaskGlyph } from './hydrating-mask-glyph.svg';
+export {
+  default as HydrocolloidBandageGlyph,
+} from './hydrocolloid-bandage-glyph.svg';
+export { default as LipBalmGlyph } from './lip-balm-glyph.svg';
 export { default as LockGlyph } from './lock-glyph.svg';
 export { default as MaleGlyph } from './male-glyph.svg';
 export { default as MessageGlyph } from './message-glyph.svg';
+export { default as MicellarWaterGlyph } from './micellar-water-glyph.svg';
 export { default as MixIngredientsGlyph } from './mix-ingredients-glyph.svg';
+export { default as MoisturizerGlyph } from './moisturizer-glyph.svg';
 export { default as MortorGlyph } from './mortor-glyph.svg';
 export { default as NeckGlyph } from './neck-glyph.svg';
 export { default as NiacinamideGlyph } from './niacinamide-glyph.svg';
@@ -49,9 +69,11 @@ export { default as NoMakeupGlyph } from './no-makeup-glyph.svg';
 export { default as NoneGlyph } from './none-glyph.svg';
 export { default as OftenDryGlyph } from './often-dry-glyph.svg';
 export { default as OftenOilyGlyph } from './often-oily-glyph.svg';
+export { default as OilCleanserGlyph } from './oil-cleanser-glyph.svg';
 export { default as OtherGlyph } from './other-glyph.svg';
 export { default as PackagingGlyph } from './packaging-glyph.svg';
 export { default as PillsGlyph } from './pills-glyph.svg';
+export { default as RichMoisturizerGlyph } from './rich-moisturizer-glyph.svg';
 export { default as RocketshipGlyph } from './rocketship-glyph.svg';
 export { default as SelfieGlyph } from './selfie-glyph.svg';
 export { default as SkinTextureGlyph } from './skin-texture-glyph.svg';
@@ -60,7 +82,9 @@ export { default as SunGlyph } from './sun-glyph.svg';
 export { default as SunblockGlyph } from './sunblock-glyph.svg';
 export { default as SunscreenBottleGlyph } from './sunscreen-bottle-glyph.svg';
 export { default as SunscreenDropGlyph } from './sunscreen-drop-glyph.svg';
+export { default as SunscreenGlyph } from './sunscreen-glyph.svg';
 export { default as SunscreenTubGlyph } from './sunscreen-tub-glyph.svg';
+export { default as SuperbottleGlyph } from './superbottle-glyph.svg';
 export { default as SupportCenterGlyph } from './support-center-glyph.svg';
 export { default as TextureGlyph } from './texture-glyph.svg';
 export { default as TimeAlwaysGlyph } from './time-always-glyph.svg';
@@ -73,6 +97,8 @@ export { default as TretinoinGlyph } from './tretinoin-glyph.svg';
 export { default as VeryDryGlyph } from './very-dry-glyph.svg';
 export { default as VeryOilyGlyph } from './very-oily-glyph.svg';
 export { default as VitaminCGlyph } from './vitamin-c-glyph.svg';
+export { default as VitaminCSerumGlyph } from './vitamin-c-serum-glyph.svg';
 export { default as WrinklesGlyph } from './wrinkles-glyph.svg';
 export { default as YesGlyph } from './yes-glyph.svg';
 export { default as ZincGlyph } from './zinc-glyph.svg';
+export { default as ZincSoapBarGlyph } from './zinc-soap-bar-glyph.svg';
