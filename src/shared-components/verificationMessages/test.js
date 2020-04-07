cb56dd13b8299f1e8258c8fca397c3a4137776f0
Matt A <9440455+matt-allan@users.noreply.github.com>
2020-04-07 12:19:30 -0400

    Allow passing an empty array of messages to VerificationMessages (#216)
    * Allow passing an empty array of messages to VerificationMessages
    
    * Update src/shared-components/verificationMessages/test.js
    
    Co-Authored-By: Diego Fortes <daigof@gmail.com>
    
    Co-authored-by: Diego Fortes <daigof@gmail.com>

---
+++ src/shared-components/verificationMessages/test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+import VerificationMessages from './index';
+
+describe('<VerificationMessages />', () => {
+  describe('UI snapshot', () => {
+    it('renders content and children', () => {
+      const messages = {
+        required: (
+          <React.Fragment>
+            <strong>Uh oh!</strong> This field is required
+          </React.Fragment>
+        ),
+        maxLength: (
+          <React.Fragment>
+            <strong>Uh oh!</strong> Must be at least 3 characters
+          </React.Fragment>
+        ),
+        charactersRequired: ['Must contain 1 number', 'Must contain 1 symbol'],
+      };
+      const component = renderer.create(
+        <VerificationMessages messages={messages} />,
+      );
+
+      const tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('when message value is an empty array', () => {
+    it('renders nothing', () => {
+      const messages = {
+        required: [],
+      };
+      const wrapper = shallow(<VerificationMessages messages={messages} />);
+      expect(wrapper.html().indexOf('li') === -1).toBe(true);
+    });
+  });
+});
