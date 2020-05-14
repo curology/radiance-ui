706a6827f591b99ebd7e69ba77bf8a229be5e504
Nate Harris <nate.harris@curology.com>
2020-05-14 12:24:51 -0700

    Deprecate SPACING

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,7 +7,6 @@ export { default as GRID } from './grid';
 export { default as LAYOUT } from './layout';
 export { default as MEDIA_QUERIES } from './mediaQueries';
 export { default as PROGRESS_BAR_STATUS } from './progressBarStatus';
-export { default as SPACING } from './spacing';
 export { default as SPACER } from './spacer';
 export { default as TYPOGRAPHY_CONSTANTS } from './typography';
 export { default as Z_SCALE } from './zScale';
