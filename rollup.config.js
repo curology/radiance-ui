498440b55aac37e71274479b3367f17821c7cd05
Tim Martin <git@timmartin.me>
2020-06-19 17:10:12 -0400

    Update rollup dependencies
    Only the babel plugin needed to be updated according to the yarn
    versions.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import svgr from '@svgr/rollup';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
