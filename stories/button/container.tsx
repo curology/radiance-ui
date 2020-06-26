f8ef615adc0bc4cdf3927ea25c0c99570d13f5cc
Diego Fortes <daigof@gmail.com>
2020-06-26 17:00:43 -0300

    button stories

---
+++ stories/button/container.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { css } from '@emotion/core';
+import { Button, Typography } from 'src/shared-components';
+
+// eslint-disable-next-line react/prop-types
+export default ({
+  title,
+  children,
+}: {
+  title?: string;
+  children: React.ReactNode;
+}) => (
+  <Button.Container
+    css={css`
+      display: block;
+      padding-bottom: 50px;
+    `}
+  >
+    <Typography.Heading
+      css={css`
+        text-align: left;
+      `}
+    >
+      {title}
+    </Typography.Heading>
+    {children}
+  </Button.Container>
+);
