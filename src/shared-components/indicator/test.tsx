004b83197da0c9c6097223aa5866512231f8e22f
Diego Fortes <daigof@gmail.com>
2020-08-17 13:49:04 -0300

    indicator conversion

--- src/shared-components/indicator/test.tsx
+++ src/shared-components/indicator/test.tsx
@@ -5,8 +5,14 @@ import Indicator from './index';
 
 describe('<Indicator />', () => {
   describe('UI snapshots', () => {
-    it('renders the correct css and text', () => {
+    it('renders the correct css with a text', () => {
       const component = renderer.create(<Indicator text="1" />);
+      const tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('renders the correct css with a number', () => {
+      const component = renderer.create(<Indicator text={3} />);
       const tree = component.toJSON();
       expect(tree).toMatchSnapshot();
     });
