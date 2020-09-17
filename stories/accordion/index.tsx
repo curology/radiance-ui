7d95b2b3a04dbea5bf8dcfd1c5fd223cdb5cea06
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-09-17 12:39:29 -0500

    Fix formatting

--- stories/accordion/index.tsx
+++ stories/accordion/index.tsx
@@ -54,27 +54,27 @@ stories.add(
             width: 400px;
           `}
         >
-                      <Accordion.Container>
-              <Accordion
-                title={
-                  <Accordion.Content>
-                    <Accordion.Truncate>
-                      {text('Title text', 'Title')}
-                    </Accordion.Truncate>
-                  </Accordion.Content>
-                }
-                onClick={action('Accordion clicked')}
-                isOpen={boolean('isOpen', false)}
-                noBorder={boolean('noBorder', false)}
-                disabled={boolean('disabled', false)}
-                rightAlignArrow={boolean('rightAlignArrow', false)}
-              >
+          <Accordion.Container>
+            <Accordion
+              title={
                 <Accordion.Content>
-                  {text('Expanded text', 'Accordion expanded content')}
+                  <Accordion.Truncate>
+                    {text('Title text', 'Title')}
+                  </Accordion.Truncate>
                 </Accordion.Content>
-              </Accordion>
-            </Accordion.Container>
-                  </div>
+              }
+              onClick={action('Accordion clicked')}
+              isOpen={boolean('isOpen', false)}
+              noBorder={boolean('noBorder', false)}
+              disabled={boolean('disabled', false)}
+              rightAlignArrow={boolean('rightAlignArrow', false)}
+            >
+              <Accordion.Content>
+                {text('Expanded text', 'Accordion expanded content')}
+              </Accordion.Content>
+            </Accordion>
+          </Accordion.Container>
+        </div>
       </div>
     </React.Fragment>
   )),
