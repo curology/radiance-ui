3cb4c7837008581bccee30ddb4336cf73094977b
Amy-Kate Andrews <amy.kate.andrews@gmail.com>
2020-09-28 11:51:12 -0600

    add variable items to example story

--- stories/segmentedControl/defaultControl/index.tsx
+++ stories/segmentedControl/defaultControl/index.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 import { css } from '@emotion/core';
 
 import { SegmentedControl } from 'src/shared-components';
+import { SegmentItemType } from 'src/shared-components/segmentedControl/types';
 
 type DefaultControlProps = {
   width?: string;
+  segmentItems: SegmentItemType[];
 };
 
-const DefaultControl: React.FC<DefaultControlProps> = ({ width = '500px' }) => {
-  const segmentItems = [
-    { id: 1, text: 'Option 1' },
-    { id: 2, text: 'Option 2' },
-    { id: 3, text: 'Option 3' },
-  ];
-
+const DefaultControl: React.FC<DefaultControlProps> = ({
+  width = '500px',
+  segmentItems,
+}) => {
   return (
     <div
       css={css`
         width: ${width};
       `}
     >
-      <SegmentedControl segmentItems={segmentItems} initialActiveId={2} />
+      <SegmentedControl segmentItems={segmentItems} initialActiveId={1} />
     </div>
   );
 };
--- stories/segmentedControl/index.tsx
+++ stories/segmentedControl/index.tsx
@@ -5,25 +5,42 @@ import { withDocs } from 'storybook-readme';
 
 import SegmentedControlReadme from 'docs/segmentedControl';
 
-import DefaultControl from './defaultControl'
+import DefaultControl from './defaultControl';
 import { css } from '@emotion/core';
 import { SPACER } from 'src/constants';
 const stories = storiesOf('SegmentedControl', module);
 
+const threeItems = [
+  { id: 1, text: 'Option 1' },
+  { id: 2, text: 'Option 2' },
+  { id: 3, text: 'Option 3' },
+];
+
+const twoItems = [
+  { id: 1, text: 'Option 1' },
+  { id: 2, text: 'Option 2' },
+];
+
 stories.addDecorator(withKnobs);
 
 stories.add(
-	'Usage',
-	withDocs(SegmentedControlReadme, () => (
-		<React.Fragment>
-			<div css={css`
-				margin-bottom: ${SPACER.small};`}>
-				<DefaultControl />
-			</div>
-			<div css={css`
-				margin-bottom: ${SPACER.small};`}>
-				<DefaultControl width="300px" />
-			</div>
-		</React.Fragment>
-	))
-)
+  'Usage',
+  withDocs(SegmentedControlReadme, () => (
+    <React.Fragment>
+      <div
+        css={css`
+          margin-bottom: ${SPACER.small};
+        `}
+      >
+        <DefaultControl segmentItems={threeItems} />
+      </div>
+      <div
+        css={css`
+          margin-bottom: ${SPACER.small};
+        `}
+      >
+        <DefaultControl width="300px" segmentItems={twoItems} />
+      </div>
+    </React.Fragment>
+  )),
+);
