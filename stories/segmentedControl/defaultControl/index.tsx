555fa60677c23bb426ec910b7e4a56b3c6c7069d
Amy-Kate Andrews <amy.kate.andrews@gmail.com>
2020-09-28 11:46:50 -0600

    add type for example element

--- stories/segmentedControl/defaultControl/index.tsx
+++ stories/segmentedControl/defaultControl/index.tsx
@@ -3,21 +3,26 @@ import { css } from '@emotion/core';
 
 import { SegmentedControl } from 'src/shared-components';
 
-const DefaultControl: React.FC = ({ width = '500px' }: {width: string}) => {
-		const segmentItems = [
-			{ id: 1, text: 'Option 1' },
-			{ id: 2, text: 'Option 2' },
-			{ id: 3, text: 'Option 3' },
-		  ];
+type DefaultControlProps = {
+  width?: string;
+};
 
-		  return (
-			  <div
-				  css={css`
-				 	width: ${width}; 
-				  `}>
-					  <SegmentedControl segmentItems={segmentItems} />
-				  </div>
-		  )
-}
+const DefaultControl: React.FC<DefaultControlProps> = ({ width = '500px' }) => {
+  const segmentItems = [
+    { id: 1, text: 'Option 1' },
+    { id: 2, text: 'Option 2' },
+    { id: 3, text: 'Option 3' },
+  ];
 
-export default DefaultControl
+  return (
+    <div
+      css={css`
+        width: ${width};
+      `}
+    >
+      <SegmentedControl segmentItems={segmentItems} initialActiveId={2} />
+    </div>
+  );
+};
+
+export default DefaultControl;
