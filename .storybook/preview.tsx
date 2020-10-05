b756b330c4ee818c9da04ed7ac55fffa88889832
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-10-05 13:10:26 -0500

    Clean up more deprecated warnings, add theming explicitly to package.json, remove @storybook/addon-docs

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,6 @@
 import { addDecorator, addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import addons from '@storybook/addons';
 import { addReadme, configureReadme } from 'storybook-readme';
 import { Global, css } from '@emotion/core';
 import Theme from './theme';
@@ -93,15 +94,24 @@ addParameters({
   },
   options: {
     theme: Theme,
-    isFullscreen: false,
-    showNav: true,
-    showPanel: true,
-    panelPosition: 'right',
-    sidebarAnimations: true,
-    enableShortcuts: true,
-    isToolshown: true,
   },
   viewport: {
     viewports: INITIAL_VIEWPORTS,
   },
 });
+
+/**
+ * {@link https://storybook.js.org/docs/react/configure/features-and-behavior Options}
+ */
+const ADDONS_CONFIG = {
+  enableShortcuts: true,
+  isFullscreen: false,
+  isToolshown: true,
+  panelPosition: 'right',
+  showNav: true,
+  showPanel: true,
+  sidebarAnimations: true,
+  theme: Theme,
+};
+
+addons.setConfig(ADDONS_CONFIG);
