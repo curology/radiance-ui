1c441710485983fed3a0e73d9efbf3902cbc8cd0
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-10-06 13:05:59 -0500

    Convert Checkbox story

--- src/shared-components/checkbox/index.js
+++ src/shared-components/checkbox/index.js
@@ -7,11 +7,17 @@ const propTypes = {
   checked: PropTypes.bool.isRequired,
   disabled: PropTypes.bool,
   onClick: PropTypes.func,
+  /**
+   * Text label displayed next to the checkbox
+   */
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
   type: PropTypes.oneOf(['primary', 'secondary']),
+  /**
+   * Icons are only displayed at the large size, and inside the radio button
+   */
   icon: PropTypes.node,
   size: PropTypes.oneOf(['large', 'small']),
 };
@@ -39,7 +45,7 @@ const Checkbox = ({
     selector="checkbox"
     checked={checked}
     disabled={disabled}
-    onClick={!disabled ? onClick : event => event.preventDefault()}
+    onClick={!disabled ? onClick : (event) => event.preventDefault()}
     type={type}
     icon={icon}
     size={size}
