815e5fae7ed50794e50075d5e4048b5dd3ea5e87
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-10-07 14:25:06 -0500

    Simplify lint-staged config with new setup

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  '!(style|styles|test).js': ['prettier --write', 'eslint --fix'],
-  '(style|styles|test).js': 'prettier --write',
-  'src/**/*.{ts,tsx}': [
+  'src/**/*.{js,ts,tsx}': [
     'prettier --write',
     'eslint --fix',
     () => 'tsc -p tsconfig.json --noEmit --emitDeclarationOnly false',
