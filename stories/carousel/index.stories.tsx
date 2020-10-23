6fc373f6a201c8f929037b7f043da1e50fd25db6
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-10-23 12:32:44 -0500

    Reconfigure janky carousel test

--- stories/carousel/index.stories.tsx
+++ stories/carousel/index.stories.tsx
@@ -10,7 +10,7 @@ import {
 } from '@storybook/addon-docs/blocks';
 import { Carousel } from 'src/shared-components';
 import { text, select, number, boolean } from '@storybook/addon-knobs';
-import { COLORS, SPACER } from 'src/constants';
+import { ANIMATION, COLORS, SPACER } from 'src/constants';
 import type { Meta } from '@storybook/react';
 
 const Card = styled(Carousel.Card)`
@@ -115,6 +115,10 @@ export default {
   title: 'Components/Carousel',
   component: Carousel,
   parameters: {
+    /**
+     * There is visual jank when this component loads--this reduces brittleness in Chromatic
+     */
+    chromatic: { delay: parseInt(ANIMATION.defaultTiming, 10) },
     docs: {
       page: () => (
         <React.Fragment>
