29936d3e8538f454f79cedf6dc4547c3fb3bbc8c
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-10-23 13:50:11 -0500

    Add new config to enhance eslint typescript capabilities

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,13 @@
 module.exports = {
-  '!(style|styles|test).js': ['prettier --write', 'eslint --fix'],
-  '(style|styles|test).js': 'prettier --write',
-  'src/**/*.{ts,tsx}': [
+  'src/**/*.{js,ts,tsx}': [
     'prettier --write',
     'eslint --fix',
     () => 'tsc -p tsconfig.json --noEmit --emitDeclarationOnly false',
   ],
-  '*.{md,json}': 'prettier --write',
+  'stories/**/*.{js,ts,tsx}': [
+    'prettier --write',
+    'eslint --fix',
+    () => 'tsc -p stories/tsconfig.json --noEmit',
+  ],
+  '*.json': 'prettier --write',
 };
