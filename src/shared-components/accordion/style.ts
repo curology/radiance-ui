82d5edfc4dbec60640db293d9315c9bcc36ff5ce
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-10-27 17:07:20 -0500

    [Theming] Add built-in TypeScript support (#463)
    * initial setup
    
    * convert Acoordion,add  theme suport to stories
    
    * complete accordion example
    
    * shouldShowForEnvironment refactor
    
    * commit snapshot size
    
    * v13.0.0-alpha.1
    
    * Commit example with Rollup + TypeScript resolution
    
    * Commit hard-coded import
    
    * Revert "Commit hard-coded import"
    
    This reverts commit 9713df23e062c7569f5ba9e3b46b581070880344.
    
    * PR Cleanup
    
    * Remove rollup plugin
    
    * Move type resolution files out of utils and into types folder
    
    * Commit master size-snapshot
    
    * Push last size-snapshot
    
    Co-authored-by: Diego Fortes <daigof@gmail.com>

--- src/shared-components/accordion/style.ts
+++ src/shared-components/accordion/style.ts
@@ -28,7 +28,6 @@ export const AccordionBox = styled.div<{
   noBorder: boolean;
   isOpen: boolean;
   disabled: boolean;
-  theme?: ThemeType;
 }>`
   ${({ noBorder, isOpen, theme }) =>
     !noBorder ? getBorderStyle(theme, isOpen) : ''};
@@ -97,7 +96,6 @@ export const Truncate = styled.div`
  */
 export const Container = styled.div<{
   borderRadius?: string;
-  theme?: ThemeType;
 }>`
   box-shadow: ${BOX_SHADOWS.clickable};
   background-color: ${({ theme }) => theme.COLORS.white};
