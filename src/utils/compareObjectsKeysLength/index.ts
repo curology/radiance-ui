4b1a5a076f3e2ec44f746cd49ba40bc0260f2f90
Diego Fortes <daigof@gmail.com>
2020-11-09 15:57:11 -0300

    reafctor util tester

--- src/utils/compareObjectsKeysLength/index.ts
+++ src/utils/compareObjectsKeysLength/index.ts
@@ -1,9 +1,6 @@
-const compareObjectsKeysLength = (object1: {}, object2: {}) => {
-  if (typeof object1 !== 'object' || typeof object2 !== 'object') {
-    return false;
-  }
-
-  return Object.keys(object1).length === Object.keys(object2).length;
-};
+const compareObjectsKeysLength = (
+  object1: Record<string, unknown>,
+  object2: Record<string, unknown>,
+) => Object.keys(object1).length === Object.keys(object2).length;
 
 export default compareObjectsKeysLength;
