6803e92ccec81b3cfea77897606615c48fb39861
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-11-11 12:13:46 -0600

    Add PRIMARY_TYPOGRAPHY and SECONDARY_TYPOGRAPHY

--- src/constants/themes/secondaryTheme.ts
+++ src/constants/themes/secondaryTheme.ts
@@ -1,9 +1,11 @@
 import SECONDARY_COLORS from '../colors/secondary';
 import SECONDARY_FONTS from '../fonts/secondary';
+import { SECONDARY_TYPOGRAPHY } from '../typography/secondaryTypography';
 
 export const secondaryTheme = {
   __type: 'secondary',
   BOX_SHADOW: {},
   COLORS: SECONDARY_COLORS,
   FONTS: SECONDARY_FONTS,
+  TYPOGRAPHY_CONSTANTS: SECONDARY_TYPOGRAPHY,
 } as const;
