c60e7c2fdd8851836b88da11b2cc85c6748e7e52
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-11-11 12:10:23 -0600

    Add recursive checking to compareObjectsKeysLength

--- src/utils/compareObjectsKeysLength/index.ts
+++ src/utils/compareObjectsKeysLength/index.ts
@@ -1,6 +1,42 @@
+// eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
+interface RecursiveObjectType {
+  [key: string]: string | number | this;
+}
+
+const stringifyObjects = (
+  obj1: RecursiveObjectType,
+  obj2: RecursiveObjectType,
+) => `\n\nobj1: ${JSON.stringify(obj1)}\n\nobj2:${JSON.stringify(obj2)}`;
+
 const compareObjectsKeysLength = (
-  object1: Record<string, unknown>,
-  object2: Record<string, unknown>,
-) => Object.keys(object1).length === Object.keys(object2).length;
+  obj1: RecursiveObjectType,
+  obj2: RecursiveObjectType,
+): boolean => {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    console.error(`Object keys do not match:${stringifyObjects(obj1, obj2)}`);
+    return false;
+  }
+
+  return Object.keys(obj1).every((key) => {
+    const obj1Val = obj1[key];
+    const obj2Val = obj2[key];
+
+    // Test deep key equality recursively
+    if (typeof obj1Val === 'object' && typeof obj2Val === 'object') {
+      return compareObjectsKeysLength(obj1Val, obj2Val);
+    }
+
+    // Check if property value is null or undefined, neither of which are valid
+    // theme values, which means there was not a complementary key found
+    if (obj2Val == null) {
+      console.error(
+        `Key "${key}" missing in obj2:${stringifyObjects(obj1, obj2)}`,
+      );
+      return false;
+    }
+
+    return true;
+  });
+};
 
 export default compareObjectsKeysLength;
