8934bd45fa306bf1d0bcf6e002685b77a93af2d1
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-11-11 12:45:09 -0600

    [Theming] Add TYPOGRAPHY_CONSTANTS (#520)
    * Add recursive checking to compareObjectsKeysLength
    
    * Add PRIMARY_TYPOGRAPHY and SECONDARY_TYPOGRAPHY
    
    * Rename to just TYPOGRAPHY, add theme test
    
    * Add TODO

--- src/constants/themes/primaryTheme.ts
+++ src/constants/themes/primaryTheme.ts
@@ -1,9 +1,11 @@
 import PRIMARY_COLORS from '../colors/primary';
 import PRIMARY_FONTS from '../fonts/primary';
+import { PRIMARY_TYPOGRAPHY } from '../typography/primaryTypography';
 
 export const primaryTheme = {
   __type: 'primary',
   BOX_SHADOW: {},
   COLORS: PRIMARY_COLORS,
   FONTS: PRIMARY_FONTS,
+  TYPOGRAPHY: PRIMARY_TYPOGRAPHY,
 } as const;
