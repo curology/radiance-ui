d6e9b4cc19e0ab0a02000282dde7402fae060f02
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-11-12 10:59:28 -0600

    First font attempt

--- src/constants/fonts/secondary.ts
+++ src/constants/fonts/secondary.ts
@@ -1,7 +1,7 @@
 const SECONDARY_FONTS = {
-  headerFont: 'fantasy, serif',
-  baseFont: 'fantasy, serif',
-  monoFont: 'fantasy, serif',
+  headerFont: `'quadrant-text', Georgia, TimesNewRoman, 'Times New Roman', Times, Baskerville, serif`,
+  baseFont: `"GreyLLWeb-Regular", 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic, Verdana, sans-serif`,
+  monoFont: `'quadrant-text-mono', Monaco, monospace`,
 } as const;
 
 export default SECONDARY_FONTS;
