e91a13c44bcc04e89872aa7bda6d3070eba7f303
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-11-13 11:12:11 -0600

    Remove deprecated icons (none in use, new major version required anyway)

--- src/utils/svgToIconTemplate/transformTemplateForUtilLocation.js
+++ src/utils/svgToIconTemplate/transformTemplateForUtilLocation.js
@@ -1,27 +1,16 @@
 const addCssJsxAttribute = require('./addCssJsxAttribute.js');
 const addRestSpreadJsxAttribute = require('./addRestSpreadJsxAttribute.js');
-const deprecatedIcons = require('../icons/deprecatedList.ts');
 
 function transformTemplateForUtilLocation(utilLocation) {
   /* eslint-disable @typescript-eslint/no-unused-vars */
   return function transformTemplate(
     { template },
     opts,
-    {
-      imports, componentName, props, jsx, exports, 
-    },
+    { imports, componentName, props, jsx, exports },
   ) {
     /* eslint-enable @typescript-eslint/no-unused-vars */
     addCssJsxAttribute(jsx);
     addRestSpreadJsxAttribute(jsx);
-
-    // componentName comes through as an AST node with "Svg" prepended
-    // normalize the name so we can look it up on our deprecation list
-    const normalizedComponentName = componentName.name.replace(/Svg/, '');
-    const deprecationMessage = deprecatedIcons[normalizedComponentName];
-    const deprecationNotice = deprecationMessage
-      ? `console.warn('[Deprecation Warning]: ${deprecationMessage}');`
-      : '';
 
     return template.ast`
       ${imports}
@@ -30,8 +19,6 @@ function transformTemplateForUtilLocation(utilLocation) {
       import { propTypes, defaultProps, iconStyles } from '${utilLocation}';
 
       const ${componentName} = (props) => {
-        ${deprecationNotice}
-
         return (
           ${jsx}
         );
