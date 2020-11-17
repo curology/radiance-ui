3e20443694721d198ef786926f2d0001cd2b11d5
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-11-17 12:20:00 -0600

    Remove SVGR icon generation scaffolding (no longer in use)

--- src/utils/svgToIconTemplate/constants.js
+++
@@ -1,5 +0,0 @@
-const UTIL_LOCATION = '../../../utils/icons';
-
-module.exports = {
-  UTIL_LOCATION,
-};
--- src/utils/svgToIconTemplate/index.js
+++
@@ -1,4 +0,0 @@
-const transformTemplateForUtilLocation = require('./transformTemplateForUtilLocation.js');
-const { UTIL_LOCATION } = require('./constants');
-
-module.exports = transformTemplateForUtilLocation(UTIL_LOCATION);
