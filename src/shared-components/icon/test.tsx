e2224866d6dd697b578e9a4d2792a9492010c5e8
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-11-18 14:15:59 -0600

    Delete unused icon test

--- src/shared-components/icon/test.tsx
+++
@@ -1,9 +0,0 @@
-describe('Icon', () => {
-  it('displays the primary icon with the primary theme', () => {
-    expect(1).toEqual(1);
-  });
-
-  it('displays the secondary icon with the secondary theme', () => {
-    expect(1).toEqual(1);
-  });
-});
