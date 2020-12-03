bbda391f8f07fc670ed77aa9a3ea468a0737fb24
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-12-03 15:47:30 -0600

    Remove enzyme deps

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,9 +1,5 @@
 import '@testing-library/jest-dom';
 import 'regenerator-runtime/runtime'; // Allows async in tests
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 window.matchMedia =
   window.matchMedia ||
