3ebf937821b5d016d982cf8b596799fc91186fad
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-12-09 11:18:37 -0600

    Restore viewBox behavior

--- src/icons/navIcons/svgs/NotificationsNavIcon.tsx
+++ src/icons/navIcons/svgs/NotificationsNavIcon.tsx
@@ -3,8 +3,7 @@ import * as React from 'react';
 function SvgNotificationsNavIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width={20}
-      height={20}
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
--- src/icons/navIcons/svgs/ProfileSelectedNavIconSecondary.tsx
+++ src/icons/navIcons/svgs/ProfileSelectedNavIconSecondary.tsx
@@ -5,8 +5,7 @@ function SvgProfileSelectedNavIconSecondary(
 ) {
   return (
     <svg
-      width={20}
-      height={20}
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
