6593070526abd4a968b67b5ac94f7fbd81d4d356
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-12-10 17:44:59 -0600

    Fix secondary heart selected SVG, make fill currentColor, keep viewBox only

--- src/icons/emojis/svgs/SmileySelectedEmojiSecondary.tsx
+++ src/icons/emojis/svgs/SmileySelectedEmojiSecondary.tsx
@@ -3,15 +3,14 @@ import * as React from 'react';
 function SvgSmileySelectedEmojiSecondary(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width={48}
-      height={48}
+      viewBox="0 0 48 48"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
         d="M23.75 8C15.07 8 8 15.07 8 23.75S15.07 39.5 23.75 39.5 39.5 32.43 39.5 23.75 32.43 8 23.75 8zm6.59 19.93c-1 3-3.6 4.9-6.66 4.9h-.24c-3.06-.1-5.53-2.04-6.3-4.94a.75.75 0 01.53-.92c.4-.1.81.13.92.54.59 2.24 2.51 3.74 4.89 3.82 1.97.05 4.45-.92 5.44-3.87.13-.39.55-.61.95-.47.39.12.61.55.47.94z"
-        fill="#2D2A26"
+        fill="currentColor"
       />
     </svg>
   );
