4d19b3305a68abc58d3072181234dc7a6d2ce30f
Diego Fortes <daigof@gmail.com>
2020-12-11 10:58:17 -0300

    refactor theme stories

---
+++ stories/theme/index.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Title,
+  Description,
+  Heading,
+  Source,
+  Stories,
+} from '@storybook/addon-docs/blocks';
+import type { Meta } from '@storybook/react';
+import { BREAKPOINTS } from 'src/constants';
+
+export * from './boxShadows.stories';
+export * from './colors.stories';
+export * from './fonts.stories';
+export * from './typography.stories';
+
+const styledComponentSource =
+  // eslint-disable-next-line no-template-curly-in-string
+  'export const StyledComponent = styled.div`\n  color: ${({ theme }) => theme.COLORS.primary};\n`';
+
+const cssPropSource =
+  // eslint-disable-next-line no-template-curly-in-string
+  '<Component css={(theme: ThemeType) => css`color: ${theme.COLORS.primary}`}>';
+
+export default {
+  title: 'Constants/Theme',
+  parameters: {
+    chromatic: { viewports: [BREAKPOINTS.xs] },
+    docs: {
+      page: () => (
+        <React.Fragment>
+          <Title />
+          <Description />
+          <Heading>Usage:</Heading>
+          <Description>
+            The `theme` object is accessible (in theme aware apps using
+            `emotion-theming`) from these 2 common contexts: Styled component
+            (as a prop) and as a React component css prop (as a function
+            parameter):
+          </Description>
+          <Source language="tsx" code={styledComponentSource} />
+          <Source language="tsx" code={cssPropSource} />
+          <Stories includePrimary />
+        </React.Fragment>
+      ),
+    },
+  },
+} as Meta;
---
+++ stories/theme/typography.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { useTheme } from 'emotion-theming';
+import { SPACER } from 'src/constants';
+import { Typography } from 'src/shared-components';
+
+const MainContainer = styled.div`
+  padding: ${SPACER.large};
+`;
+
+const CategoryContainer = styled.div`
+  margin-bottom: ${SPACER.medium};
+`;
+
+export const TYPOGRAPHY = () => {
+  const theme = useTheme();
+  const typographyCategoriesKeys = Object.keys(theme.TYPOGRAPHY) as Array<
+    keyof typeof theme['TYPOGRAPHY']
+  >;
+
+  return (
+    <MainContainer>
+      {typographyCategoriesKeys.map((typographyCategoryKey) => {
+        const typographyKeys = Object.keys(
+          theme.TYPOGRAPHY[typographyCategoryKey],
+        );
+
+        return (
+          <CategoryContainer key={typographyCategoryKey}>
+            <Typography.Title>{typographyCategoryKey}</Typography.Title>
+            {typographyKeys.map((typographyKey) => {
+              const typographyValue = theme.TYPOGRAPHY[typographyCategoryKey][
+                typographyKey
+              ] as string;
+
+              return (
+                <p key={typographyKey}>
+                  <strong>{typographyKey}</strong>: {typographyValue}
+                </p>
+              );
+            })}
+          </CategoryContainer>
+        );
+      })}
+    </MainContainer>
+  );
+};
