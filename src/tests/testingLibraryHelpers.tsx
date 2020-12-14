3831d5cb0970c66a39e6dc5d489d89545d40de40
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-12-14 14:35:48 -0600

    Expand custom render logic with comments, merge theme into options

--- src/tests/testingLibraryHelpers.tsx
+++ src/tests/testingLibraryHelpers.tsx
@@ -4,16 +4,26 @@ import * as ReactTestingLibrary from '@testing-library/react';
 
 import { primaryTheme, ThemeType } from '../constants';
 
-// We customize @testing-library methods to bake-in theming and keep unit tests DRY
+interface RenderOptions extends ReactTestingLibrary.RenderOptions {
+  theme?: ThemeType;
+}
+
+// We customize @testing-library methods to bake-in theming and keep unit tests DRY.
+// We do not use built-in { wrapper } option because it causes an inscrutable error.
+// <ThemeProvider> leaves no DOM trace, anyway, so there's no functional difference.
 const customRender = (
   Component: React.ReactElement,
-  options?: Omit<ReactTestingLibrary.RenderOptions, 'queries'>,
-  theme: ThemeType = primaryTheme,
-): ReactTestingLibrary.RenderResult =>
-  ReactTestingLibrary.render(
+  options: RenderOptions = {},
+): ReactTestingLibrary.RenderResult => {
+  const { theme = primaryTheme, ...rest } = options;
+
+  return ReactTestingLibrary.render(
     <ThemeProvider theme={theme}>{Component}</ThemeProvider>,
-    options,
+    {
+      ...rest,
+    },
   );
+};
 
 export * from '@testing-library/react';
 export { default as userEvent } from '@testing-library/user-event';
