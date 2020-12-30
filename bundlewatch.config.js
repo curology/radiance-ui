c96e1eca35955caa4503dbfa3c88bd8efaea796e
Michael Altamirano <michaeljaltamirano@gmail.com>
2020-12-30 17:17:39 -0600

    Remove compression, separate Typography component code from other component code

--- bundlewatch.config.js
+++ bundlewatch.config.js
@@ -2,11 +2,18 @@ module.exports = {
   files: [
     {
       path: 'dist/bundle.js',
-      maxSize: '100kB',
+      maxSize: '1000kB',
+      compression: 'none',
     },
     {
-      path: `dist/bundle-es/shared-components/**/index.js`,
-      maxSize: '5kB',
+      path: `dist/bundle-es/shared-components/!(typography)/index.js`,
+      maxSize: '20kB',
+      compression: 'none',
+    },
+    {
+      path: `dist/bundle-es/shared-components/typography/index.js`,
+      maxSize: '100kB',
+      compression: 'none',
     },
   ],
 };
