b08694dea8ff1a20f4a752bf971e85d9e43414a0
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-02-15 11:21:41 -0600

    Remove superfluous variable

--- src/shared-components/button/components/anchorLinkButton/index.tsx
+++ src/shared-components/button/components/anchorLinkButton/index.tsx
@@ -18,33 +18,31 @@ type ButtonRefType =
   | null
   | undefined;
 
-const AnchorLinkButton = (props: AnchorLinkButtonProps, ref: ButtonRefType) => {
-  const { children, onClick } = props;
-
-  return (
-    <button
-      ref={ref}
-      css={Style.anchorLinkButton}
-      onClick={onClick}
-      type="button"
-    >
-      {children}
-    </button>
-  );
-};
-
 /**
- * `AnchorLinkButton` will render an `anchor-link-like' button for triggering changes to the page, typically initiating modals.
+ * `AnchorLinkButton` will render an `anchor-link-like` button for triggering changes to the page, typically initiating modals.
  *
  * It should not be used for navigation: see `LinkButton` documentation for navigation functionality.
  *
  * It is the only "Button" that does not extend the functionality/styling of our base Button component.
  */
-const AnchorLinkButtonWithRef = React.forwardRef(AnchorLinkButton);
+export const AnchorLinkButton = React.forwardRef(
+  (props: AnchorLinkButtonProps, ref: ButtonRefType) => {
+    const { children, onClick } = props;
 
-AnchorLinkButtonWithRef.propTypes = {
+    return (
+      <button
+        ref={ref}
+        css={Style.anchorLinkButton}
+        onClick={onClick}
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+AnchorLinkButton.propTypes = {
   children: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
 };
-
-export { AnchorLinkButtonWithRef as AnchorLinkButton };
