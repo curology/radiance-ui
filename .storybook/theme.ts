fb2a3a6e549ac93a19a4501b0cda2902942180df
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-02-16 13:09:22 -0600

    Reset theme changes, but keep font toggling

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -1,57 +1,44 @@
 import { create } from '@storybook/theming';
-import { primaryTheme, secondaryTheme } from '../src/constants/themes';
+import { primaryTheme } from '../src/constants/themes';
 
+/**
+ * TODO-MA: Add theme support/toggling for Storybook Theme
+ */
 const Theme = create({
   base: 'light',
 
-  // colorPrimary: '#FF4785',
-  colorPrimary: primaryTheme.COLORS.primary,
-  // colorSecondary: '#1EA7FD',
-  colorSecondary: primaryTheme.COLORS.secondary,
+  colorPrimary: '#FF4785',
+  colorSecondary: '#1EA7FD',
 
   // UI
-  // appBg: '#F6F9FC',
-  // appBorderColor: 'rgba(0,0,0,.1)',
-  // appBorderRadius: 4,
-  // appContentBg: '#FFFFFF',
-  appBg: primaryTheme.COLORS.background,
-  appBorderColor: primaryTheme.COLORS.border,
-  appBorderRadius: parseInt(primaryTheme.BORDER_RADIUS.small),
-  appContentBg: primaryTheme.COLORS.white,
+  appBg: '#F6F9FC',
+  appBorderColor: 'rgba(0,0,0,.1)',
+  appBorderRadius: 4,
+  appContentBg: '#FFFFFF',
 
   // Form colors
-  // inputBg: '#FFFFFF',
-  // inputBorder: 'rgba(0,0,0,.1)',
-  // inputBorderRadius: 4,
-  // inputTextColor: '#333333',
-  inputBg: primaryTheme.COLORS.white,
-  inputBorder: 'purple',
-  inputBorderRadius: parseInt(primaryTheme.BORDER_RADIUS.small),
-  inputTextColor: primaryTheme.COLORS.black,
+  inputBg: '#FFFFFF',
+  inputBorder: 'rgba(0,0,0,.1)',
+  inputBorderRadius: 4,
+  inputTextColor: '#333333',
 
   // Toolbar default and active colors
-  // barBg: '#FFFFFF',
-  // barTextColor: '#999999',
-  // barSelectedColor: '#1EA7FD',
-  barBg: primaryTheme.COLORS.white,
-  barTextColor: 'yellow',
-  barSelectedColor: 'pink',
+  barBg: '#FFFFFF',
+  barTextColor: '#999999',
+  barSelectedColor: '#1EA7FD',
 
   // Typography
-  // fontBase: `"Nunito Sans", -apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif`,
   fontBase:
     primaryTheme.FONTS.baseFont +
     `"Nunito Sans", -apple-system, ".SFNSText-Regular", "San Francisco", BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif`,
   fontCode: `"Operator Mono", "Fira Code Retina", "Fira Code", "FiraCode-Retina", "Andale Mono", "Lucida Console", Consolas, Monaco, monospace`,
 
   // Text colors
-  textColor: primaryTheme.COLORS.black,
-  textInverseColor: primaryTheme.COLORS.white,
-  textMutedColor: 'pink',
+  textColor: '#333333',
+  textInverseColor: '#FFFFFF',
 
-  brandTitle: 'Radiance UI Redux',
+  brandTitle: 'Radiance UI',
   brandUrl: 'https://www.curology.com',
-  gridCellSize: 100,
 });
 
 export default Theme;
