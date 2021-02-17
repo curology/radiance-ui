6c77f54186304bd23eb0cba8c417fa283143af2b
Diego Fortes <daigof@gmail.com>
2021-02-17 17:04:33 -0300

    add icons

---
+++ src/icons/glyphs/greenTeaGlyph.ts
@@ -0,0 +1,5 @@
+import { GreenTeaGlyphSecondary } from './svgs';
+import { useIcon, IconProps } from '../../shared-components/icon';
+
+export default (props: IconProps) =>
+  useIcon(null, GreenTeaGlyphSecondary, props);
---
+++ src/icons/glyphs/resveratrolGlyph.ts
@@ -0,0 +1,5 @@
+import { ResveratrolGlyphSecondary } from './svgs';
+import { useIcon, IconProps } from '../../shared-components/icon';
+
+export default (props: IconProps) =>
+  useIcon(null, ResveratrolGlyphSecondary, props);
