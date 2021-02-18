30c4bdb33144e281dbdfa187ea91ee92859ab6ba
Bria Fincher <bria.fincher@curology.com>
2021-02-18 12:18:25 -0800

    Add test for adjusting height with expandable children

--- src/shared-components/accordion/test.tsx
+++ src/shared-components/accordion/test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render, userEvent } from 'src/tests/testingLibraryHelpers';
 
 import { Accordion } from './index';
@@ -8,6 +8,30 @@ const testAccordionProps = {
   isOpen: false,
   onClick: (): void => undefined,
   children: <div>expansion</div>,
+};
+
+const TestAccordionExpansion = () => {
+  const [isOuterOpen, setIsOuterOpen] = useState(false);
+  const [isInnerOpen, setIsInnerOpen] = useState(false);
+
+  return (
+    <Accordion
+      title={<div>title</div>}
+      isOpen={isOuterOpen}
+      onClick={() => setIsOuterOpen(true)}
+    >
+      <div style={{ height: '200px' }}>I should be visible!</div>
+
+      <Accordion
+        title={<div>title</div>}
+        isOpen={isInnerOpen}
+        onClick={() => setIsInnerOpen(true)}
+      >
+        <div style={{ height: '1000px' }} />
+      </Accordion>
+
+    </Accordion>
+  );
 };
 
 describe('<Accordion />', () => {
@@ -43,5 +67,19 @@ describe('<Accordion />', () => {
     userEvent.click(getByRole('button'));
 
     expect(spy).toHaveBeenCalled();
+  });
+
+  test('updates height when child component height changes', () => {
+    const container = render(<TestAccordionExpansion />);
+
+    // Expect container height to equal that of the outer closed accordion
+
+    // Open the outer accordion
+
+    // Expect the container height to equal outer accordion div text + inner closed accordion
+
+    // Open inner accordion
+
+    // Expect container height to equal outer accordion div text + inner accordion div text
   });
 });
