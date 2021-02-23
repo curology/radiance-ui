9c897c4cb1a1fd55fe29b55d75bc368498d9ab6e
Diego Fortes <daigof@gmail.com>
2021-02-23 15:24:12 -0300

    add icons ingredients (#815)

---
+++ src/icons/glyphs/svgs/TranexamicAcidGlyph.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+function SvgTranexamicAcidGlyph(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={48}
+      height={48}
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M24.03 17.12l-2.03.3.78 5.09-1.22.19-.78-5.09-2.03.31-.17-1.14 5.28-.8.17 1.14zM30.25 21.37l-1.33.2-.73-1.3-2.48.38-.31 1.46-1.33.2 1.46-6.6 1.34-.2 3.38 5.86zm-3.87-3.9l-.44 2.06 1.71-.26-1.04-1.84-.17-.33-.06.37z"
+        fill="currentColor"
+      />
+      <path
+        d="M8 23.02L17 10l15.8 1.05 6.9 14.48-8.95 13.15-15.94-1.23L8 23.02zm7.66 13.26l14.52 1.12 8.15-11.98-6.28-13.18-14.39-.96-8.2 11.86 6.2 13.14z"
+        fill="currentColor"
+      />
+      <path
+        d="M16.21 30.11l-2.7-6.3 6.96 1.16-4.26 5.14zm-.65-4.69l.99 2.32 1.57-1.89-2.56-.43zM27.69 34.05L22.4 27.6l8.53-1-3.24 7.45zm-2.88-5.47l2.54 3.1 1.56-3.58-4.1.48z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+export default SvgTranexamicAcidGlyph;
