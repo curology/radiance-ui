b352dc1b912d4496cf36ea1eb74b6bf4a70a7f0e
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-03-24 16:45:02 -0500

    Add secondary logo

--- src/icons/logos/logo.ts
+++ src/icons/logos/logo.ts
@@ -1,4 +1,4 @@
-import { LogoPrimary } from './svgs';
+import { LogoPrimary, LogoSecondary } from './svgs';
 import { IconProps, useIcon } from '../../shared-components/icon';
 
-export default (props: IconProps) => useIcon(LogoPrimary, LogoPrimary, props);
+export default (props: IconProps) => useIcon(LogoPrimary, LogoSecondary, props);
--- src/icons/logos/svgs/index.tsx
+++ src/icons/logos/svgs/index.tsx
@@ -1,2 +1,3 @@
 export { default as LogoPrimary } from './LogoPrimary'
+export { default as LogoSecondary } from './LogoSecondary'
 export { default as WordmarkPrimary } from './WordmarkPrimary'
