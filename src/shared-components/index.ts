b6e203d504735668542b1793539fc3b14c99e32e
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-03-30 13:20:13 -0500

    Export IconProps type

--- src/shared-components/index.ts
+++ src/shared-components/index.ts
@@ -11,6 +11,7 @@ export * from './container';
 export * from './dialogModal';
 export * from './dropdown';
 export * from './field';
+export type { IconProps } from './icon';
 export * from './immersiveModal';
 export * from './indicator';
 export * from './loadingSpinner';
