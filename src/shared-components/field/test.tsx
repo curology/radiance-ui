270ebdf06caae99fa4e1ba66602420c18f856563
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-04-05 17:53:48 -0500

    Test out what happens when we remove the initial snapshots, to see if the other new snapshots now break

--- src/shared-components/field/test.tsx
+++ src/shared-components/field/test.tsx
@@ -5,27 +5,29 @@ import { Field } from './index';
 
 describe('<Field />', () => {
   describe('UI Snapshot', () => {
-    it('renders with default props', () => {
-      const { container } = render(
-        <Field>
-          <Field.Input />
-        </Field>,
-      );
+    /* eslint-disable */
+    // it('renders with default props', () => {
+    //   const { container } = render(
+    //     <Field>
+    //       <Field.Input />
+    //     </Field>,
+    //   );
 
-      expect(container.firstElementChild).toMatchSnapshot();
-    });
+    //   expect(container.firstElementChild).toMatchSnapshot();
+    // });
 
-    it('renders with label and labelFor props', () => {
-      const labelText = 'Test Label';
-      const labelFor = 'for-input-id';
-      const { container } = render(
-        <Field label={labelText} labelFor={labelFor}>
-          <Field.Input />
-        </Field>,
-      );
+    // it('renders with label and labelFor props', () => {
+    //   const labelText = 'Test Label';
+    //   const labelFor = 'for-input-id';
+    //   const { container } = render(
+    //     <Field label={labelText} labelFor={labelFor}>
+    //       <Field.Input />
+    //     </Field>,
+    //   );
 
-      expect(container.firstElementChild).toMatchSnapshot();
-    });
+    //   expect(container.firstElementChild).toMatchSnapshot();
+    // });
+    /* eslint-enable */
 
     it('renders with errorMessage, hintMessage and hideMessagesIcon props', () => {
       const errorMessage = (
