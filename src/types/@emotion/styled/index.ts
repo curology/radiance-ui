09fe175da770c912cd27a77f3d7880f0fc3ea9df
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-05-14 12:49:54 -0500

    Move JSDoc block to match import

--- src/types/@emotion/styled/index.ts
+++ src/types/@emotion/styled/index.ts
@@ -1,15 +1,15 @@
-import styled, {
-  CreateStyled,
-  StyledComponent,
-} from '../../../../node_modules/@emotion/styled';
-
-import type { ThemeType } from '../../../constants/themes/types';
 /**
  * We rely on TypeScript path aliasing in order to decorate our `@emotion/styled` import
  * with built-in type information, so for this one import we need to use relative imports
  *
  * @see: https://emotion.sh/docs/typescript#define-a-theme
  */
+import styled, {
+  CreateStyled,
+  StyledComponent,
+} from '../../../../node_modules/@emotion/styled';
+
+import type { ThemeType } from '../../../constants/themes/types';
 
 export default styled as CreateStyled<ThemeType>;
 
