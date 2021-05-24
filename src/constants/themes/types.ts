56de293cb9fd6aeb1391a3d5306b1c1ddb1175cb
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-05-24 12:24:26 -0500

    Update BORDER_RADIUS_PROP_TYPES internals

--- src/constants/themes/types.ts
+++ src/constants/themes/types.ts
@@ -7,14 +7,13 @@ type BorderRadius =
   | typeof primaryTheme['BORDER_RADIUS']
   | typeof secondaryTheme['BORDER_RADIUS'];
 
-export const BORDER_RADIUS_PROP_TYPES = PropTypes.oneOf([
-  ...(Object.keys(primaryTheme.BORDER_RADIUS) as Array<
+export const BORDER_RADIUS_PROP_TYPES = PropTypes.oneOf(
+  // Since usage relies on keys, we only need to pull
+  // the keys from one theme rather than all of them
+  Object.keys(primaryTheme.BORDER_RADIUS) as Array<
     keyof typeof primaryTheme.BORDER_RADIUS
-  >),
-  ...(Object.keys(secondaryTheme.BORDER_RADIUS) as Array<
-    keyof typeof secondaryTheme.BORDER_RADIUS
-  >),
-]);
+  >,
+);
 
 type BoxShadows =
   | typeof primaryTheme['BOX_SHADOWS']
