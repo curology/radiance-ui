d1d348a0a4cddb31244d4507dd32a087c8dc2eae
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-05-27 10:35:31 -0500

    Add additional plugin to try and fix build step

--- babel.config.js
+++ babel.config.js
@@ -23,6 +23,7 @@ const plugins = [
   ['@babel/plugin-proposal-class-properties', { loose: true }],
   ['@babel/plugin-transform-parameters', { loose: true }],
   ['@babel/plugin-proposal-private-methods', { loose: true }],
+  ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
 ];
 
 module.exports = {
