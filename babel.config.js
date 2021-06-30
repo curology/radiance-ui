6f95fdc7587435b17075936f801b92a8109764f7
Matthew Allan <matt@mattallan.me>
2021-06-30 12:17:53 -0400

    Use { modules: auto } in all environments
    This was only necessary before because we were running Babel without
    Rollup. Now that we're using @babel/runtime which uses deep imports,
    setting { modules: false} was breaking Storybook, which needs to
    build our code both for CJS and ESM.

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,5 @@
-/**
- * We want test config to match non-test config as much as possible, but require
- * a different modules setting for env for jest to understand import statements
- *
- * @param {false | 'auto'} modules
- * @see https://babeljs.io/docs/en/babel-preset-env#modules
- */
-const getPresets = (modules) => [
-  [
-    '@babel/preset-env',
-    {
-      modules,
-    },
-  ],
+const presets = [
+  '@babel/preset-env',
   '@babel/react',
   '@babel/preset-typescript',
   [
@@ -36,12 +24,6 @@ const plugins = [
 ];
 
 module.exports = {
-  presets: getPresets(false),
+  presets,
   plugins,
-  env: {
-    test: {
-      presets: getPresets('auto'),
-      plugins,
-    },
-  },
 };
