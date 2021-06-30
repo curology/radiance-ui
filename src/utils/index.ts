348dc97e0fc06ea8cee64306ddca787257d045d4
Matthew Allan <matt@mattallan.me>
2021-06-30 10:26:20 -0400

    Export `injectGlobalStyles`
    This is necessary to use the themes, so we need to export it.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export { default as throwOnUndefinedProperty } from './throwOnUndefinedProperty'
 export { default as withDeprecationWarning } from './withDeprecationWarning';
 export { default as isObject } from './isObject';
 export { applyPrimaryThemeVerticalOffset } from './themeStyles';
+export * from './injectGlobalStyles/style';
