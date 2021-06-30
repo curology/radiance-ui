b5395e660e73d1e693bd5dd2fd7faed7c9bd0eed
Matthew Allan <matt@mattallan.me>
2021-06-30 14:37:37 -0400

    Export themes to JSON during the build
    This makes it a lot easier to use the theme constants in environments
    that can't evaluate JavaScript (other languages, SASS products, etc.)

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import svgr from '@svgr/rollup';
 import { defineConfig } from 'rollup'
+import path from 'path';
 
 import pkg from './package.json';
 
@@ -10,6 +11,40 @@ const extensions = ['.js', '.ts', '.tsx'];
 const defaultOutputOptions = {
   dir: 'dist',
   exports: 'named',
+};
+
+/**
+ * A simple Rollup plugin to write our themes to a JSON file.
+ *
+ * @type {import('rollup').Plugin}
+ */
+const themeJson = {
+  name: 'theme-json',
+  generateBundle: async function (options, bundle) {
+    const { dir, format } = options;
+
+    if (format !== 'cjs') {
+      return;
+    }
+
+    const { 'constants/index.cjs': file } = bundle;
+
+    if (!dir || !file) {
+      throw new Error('Could not locate the constants entrypoint.');
+    }
+
+    const mod = await import(path.resolve(dir, file.fileName));
+
+    const { primaryTheme, secondaryTheme } = mod;
+
+    const json = JSON.stringify({ primaryTheme, secondaryTheme }, null, 2);
+
+    this.emitFile({
+      type: 'asset',
+      fileName: 'theme.json',
+      source: json,
+    })
+  },
 };
 
 export default defineConfig({
@@ -41,6 +76,7 @@ export default defineConfig({
       extensions,
       exclude: 'node_modules/**',
     }),
+    themeJson,
   ],
   external: Object.keys(pkg.dependencies).map((name) => new RegExp(`^${name}`)),
 });
