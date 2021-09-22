b536f89ef2c75249b6e78db4a07eba051577e144
Justin <j6shieh@gmail.com>
2021-09-22 15:56:15 -0700

    add cleanse, hydrate, and protect glyphs

---
+++ src/icons/glyphs/cleanseGlyph.ts
@@ -0,0 +1,4 @@
+import { CleanseGlyph } from './svgs';
+import { useIcon, IconProps } from '../../shared-components/icon';
+
+export default (props: IconProps) => useIcon(CleanseGlyph, CleanseGlyph, props);
