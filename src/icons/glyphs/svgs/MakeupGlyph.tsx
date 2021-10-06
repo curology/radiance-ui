8fbba99a5032f2591f842fb0692c1fb8ee522d7c
Joey Nichols <joeyquarters@gmail.com>
2021-10-06 15:31:52 -0700

    Add makeup glyph (#1199)
    * Add makeup glyph
    
    * Add title to SVG for accessibility
    
    * Update design with more centered glyph

---
+++ src/icons/glyphs/svgs/MakeupGlyph.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+function SvgMakeupGlyph(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={48}
+      height={48}
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M31.952 13.25h-1.755v1.948h3.053v1.25h-3.053v1.838h3.053v1.25h-3.053v2.168h1.755v1.25h-1.755v4.888h-1.25v-4.888h-1.755v-1.25h1.755v-2.168h-3.054v-1.25h3.054v-1.838h-3.054v-1.25h3.054V13.25h-1.755V12h4.76v1.25zm-12.225 9.41v12.02h-3.679V22.66h3.679zm1.298-1.434H14.75v14.776h6.275V21.226zm10.386 8.05v5.403h-3.57v-5.403h3.57zm1.298-1.433h-6.275v8.16h6.275v-8.16z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+export default SvgMakeupGlyph;
