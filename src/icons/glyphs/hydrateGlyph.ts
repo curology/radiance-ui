4e9c52a5ebc4d674963dd66f1e7ec2bce56cb039
Justin Shieh <justin.shieh@curology.com>
2021-10-06 12:40:33 -0700

    Add cleanse, hydrate, and protect glyphs [PX-51] (#1175)
    * add cleanse, hydrate, and protect glyphs
    
    * order exports
    
    * update glyphs
    
    * add cleanse, hydrate, and protect glyphs
    
    * order exports
    
    * update glyphs

---
+++ src/icons/glyphs/hydrateGlyph.ts
@@ -0,0 +1,4 @@
+import { HydrateGlyph } from './svgs';
+import { useIcon, IconProps } from '../../shared-components/icon';
+
+export default (props: IconProps) => useIcon(HydrateGlyph, HydrateGlyph, props);
