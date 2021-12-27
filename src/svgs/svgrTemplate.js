a90f7eb410943ccf80cae4ca7351f21050799f37
Michael Altamirano <michaeljaltamirano@gmail.com>
2021-12-27 17:07:28 -0600

    Expand custom template to preserve v5 behavior

--- src/svgs/svgrTemplate.js
+++ src/svgs/svgrTemplate.js
@@ -4,6 +4,12 @@
  * @see https://react-svgr.com/docs/migrate/#template
  */
 const template = (variables, { tpl }) => {
+  variables.jsx.children.forEach((child) => {
+    if (child.openingElement?.name.name === 'title') {
+      transformJSXExpressionContainerToJSXText(child);
+    }
+  });
+
   return tpl`
 ${variables.imports};
 
@@ -18,3 +24,39 @@ ${variables.exports};
 };
 
 module.exports = template;
+
+/**
+ * We transform the default SVGR title prop behavior to output
+ * a format consistent with our JSX markup style.
+ *
+ * Before:
+ *
+ * children: [
+ *   JSXExpressionContainer {
+ *     type: "JSXExpressionContainer"
+ *     expression: {
+ *       type: "Literal"
+ *       value: 'Lock Glyph"
+ *     }
+ *   }
+ * ]
+ *
+ * <title>{'Lock Glyph'}</title>
+ *
+ * After:
+ *
+ * children: [
+ *   JSXText {
+ *     type: "JSXText"
+ *     value: 'Lock Glyph"
+ *   }
+ * ]
+ *
+ * <title>Lock Glyph</title>
+ */
+function transformJSXExpressionContainerToJSXText(child) {
+  // e.g. [{ type: 'JSXExpressionContainer', expression: { type: 'StringLiteral', value: 'Curology Logo' }}]
+  const prevChild = child.children[0];
+
+  child.children = [{ type: 'JSXText', value: prevChild.expression.value }];
+}
