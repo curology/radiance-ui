13a1785afd8afc02047d7db2ea3c490f4d2718ec
Sara Brandes <sara.brandes@curology.com>
2022-05-18 10:37:50 -0700

    update theme types

--- src/constants/themes/types.ts
+++ src/constants/themes/types.ts
@@ -2,10 +2,12 @@ import PropTypes from 'prop-types';
 
 import { primaryTheme } from './primaryTheme';
 import { secondaryTheme } from './secondaryTheme';
+import { tertiaryTheme } from './tertiaryTheme';
 
 type BorderRadius =
   | typeof primaryTheme['BORDER_RADIUS']
-  | typeof secondaryTheme['BORDER_RADIUS'];
+  | typeof secondaryTheme['BORDER_RADIUS']
+  | typeof tertiaryTheme['BORDER_RADIUS'];
 
 export const BORDER_RADIUS_PROP_TYPES = PropTypes.oneOf(
   /*
@@ -19,23 +21,30 @@ export const BORDER_RADIUS_PROP_TYPES = PropTypes.oneOf(
 
 type BoxShadows =
   | typeof primaryTheme['BOX_SHADOWS']
-  | typeof secondaryTheme['BOX_SHADOWS'];
+  | typeof secondaryTheme['BOX_SHADOWS']
+  | typeof tertiaryTheme['BOX_SHADOWS'];
 
 export type Colors =
   | typeof primaryTheme['COLORS']
-  | typeof secondaryTheme['COLORS'];
+  | typeof secondaryTheme['COLORS']
+  | typeof tertiaryTheme['COLORS'];
 
-type Fonts = typeof primaryTheme['FONTS'] | typeof secondaryTheme['FONTS'];
+type Fonts =
+  | typeof primaryTheme['FONTS']
+  | typeof secondaryTheme['FONTS']
+  | typeof tertiaryTheme['FONTS'];
 
 type Typography =
   | typeof primaryTheme['TYPOGRAPHY']
-  | typeof secondaryTheme['TYPOGRAPHY'];
+  | typeof secondaryTheme['TYPOGRAPHY']
+  | typeof tertiaryTheme['TYPOGRAPHY'];
 
 export type ThemeColors = valueof<Colors>;
 
 export const COLORS_PROP_TYPES = PropTypes.oneOf([
   ...Object.values(primaryTheme.COLORS),
   ...Object.values(secondaryTheme.COLORS),
+  ...Object.values(tertiaryTheme.COLORS)
 ]);
 
 export interface ThemeType {
@@ -44,5 +53,5 @@ export interface ThemeType {
   COLORS: Colors;
   FONTS: Fonts;
   TYPOGRAPHY: Typography;
-  __type: 'primary' | 'secondary';
+  __type: 'primary' | 'secondary' | 'tertiary';
 }
