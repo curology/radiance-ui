401eca22f0c276f15bd6c73c597a321367451811
Cheri Allen <cherimarie@gmail.com>
2023-07-19 11:39:23 -0700

    Add and use skintype display

--- src/constants/fonts/primary.ts
+++ src/constants/fonts/primary.ts
@@ -1,8 +1,8 @@
 const PRIMARY_FONTS = {
   headerFont:
-    "'nocturno', Georgia, TimesNewRoman, 'Times New Roman', Times, Baskerville, serif",
+    "skintype-display, Georgia, TimesNewRoman, 'Times New Roman', Times, Baskerville, serif",
   baseFont:
-    "'skintype-text', 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic, Verdana, sans-serif",
+    "skintype-text, 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic, Verdana, sans-serif",
   monoFont: "'apercu mono', 'Lucida Console', Monaco, monospace",
 } as const;
 
--- src/constants/fonts/tertiary.ts
+++ src/constants/fonts/tertiary.ts
@@ -1,8 +1,7 @@
 const TERTIARY_FONTS = {
   headerFont:
-    "'skintype-text', 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic, Verdana, sans-serif",
-  baseFont:
-    "'atkinson-hyperlegible', sans-serif",
+    "skintype-display, 'Avant Garde', Avantgarde, 'Century Gothic', CenturyGothic, AppleGothic, Verdana, sans-serif",
+  baseFont: "'atkinson-hyperlegible', sans-serif",
   monoFont: "'apercu mono', 'Lucida Console', Monaco, monospace",
 } as const;
 
