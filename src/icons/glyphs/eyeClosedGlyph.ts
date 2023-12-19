c53c323a0d3f7a401207dfc1d75ab90d3e244c82
Elysia Gabe <elysiagabe@gmail.com>
2023-12-19 07:58:43 -1000

    Small update

--- src/icons/glyphs/eyeClosedGlyph.ts
+++ src/icons/glyphs/eyeClosedGlyph.ts
@@ -1,5 +1,5 @@
 import { EyeClosedGlyph } from './svgs';
-import { IconProps, useIcon } from '../../shared-components/icon';
+import { type IconProps, useIcon } from '../../shared-components/icon';
 
 export default (props: IconProps) =>
   useIcon(EyeClosedGlyph, EyeClosedGlyph, props);
--- src/icons/glyphs/glycerinGlyph.tsx
+++ src/icons/glyphs/glycerinGlyph.tsx
@@ -1,4 +1,4 @@
 import { GlycerinGlyph } from './svgs';
-import { IconProps, useIcon } from '../../shared-components/icon';
+import { type IconProps, useIcon } from '../../shared-components/icon';
 
 export default (props: IconProps) => useIcon(null, GlycerinGlyph, props);
